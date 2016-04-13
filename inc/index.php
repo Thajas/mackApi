a95ba6b5f5533e7381fcccd79654f3b89b4527b6
Monty <mackraja@users.noreply.github.com>
2016-04-13 13:59:06 +0530

    Update index.php

--- inc/index.php
+++ inc/index.php
@@ -3,6 +3,6 @@ defined('ACCESS') OR exit('No direct script access allowed');
 // error_reporting(E_ALL | E_STRICT); // E_ERROR | E_CORE_ERROR | E_COMPILE_ERROR E_ALL | E_STRICT
 // ini_set('display_errors', 'on');
 require dirname ( __DIR__ ) . '/config/paths.php';
-require LIBRARY . 'SafePDO.inc';
-require COMMANDS . $object->getClassFolder () . '.inc';
-require CLASSES . $object->getClassFolder () . DS . $object->getClassFolder () . '.inc';
+require LIBRARY . 'SafePDO.php';
+require COMMANDS . $object->getClassFolder () . '.php';
+require CLASSES . $object->getClassFolder () . DS . $object->getClassFolder () . '.php';
